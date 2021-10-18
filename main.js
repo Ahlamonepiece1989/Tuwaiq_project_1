ef79c936ba7f88a90176b725003d750c20c70938
Ahlamonepiece1989 <Ahlam.Daham.rae.js@tuwaiq.edu.sa>
2021-10-18 22:02:54 +0300

    Tuwaq_project_1

---
+++ main.js
@@ -0,0 +1,6 @@
+function show_hide(){
+    if(document.getElementById('si').style.display == 'none')
+        document.getElementById('si').style.display = 'block';
+    else 
+        document.getElementById('si').style.display = 'none';
+}
